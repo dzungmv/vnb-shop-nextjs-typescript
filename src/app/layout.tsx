32769e29a236f560b626d25574263050d140da1f
jungjung261 <dungmv.now@gmail.com>
2023-04-29 20:49:53 +0700

    add facebook tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,18 +29,16 @@ export const metadata = {
         title: 'ShopVNB - Badminton Store System',
         description:
             'VNB Sports is a badminton store system with more than 50 branches nationwide, providing wholesale and retail badminton equipment from movement to professional.',
-        images: ['thumbnail.jpeg'],
+        image: 'thumbnail.jpeg',
     },
-    other: {
-        facebook: {
-            card: 'summary_large_image',
-            site: '@shopvnb',
-            creator: '@shopvnb',
-            title: 'ShopVNB - Badminton Store System',
-            description:
-                'VNB Sports is a badminton store system with more than 50 branches nationwide, providing wholesale and retail badminton equipment from movement to professional.',
-            images: ['thumbnail.jpeg'],
-        },
+    facebook: {
+        card: 'summary_large_image',
+        site: '@shopvnb',
+        creator: '@shopvnb',
+        title: 'ShopVNB - Badminton Store System',
+        description:
+            'VNB Sports is a badminton store system with more than 50 branches nationwide, providing wholesale and retail badminton equipment from movement to professional.',
+        images: 'thumbnail.jpeg',
     },
 };
 
